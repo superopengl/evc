d58cc1c60f5d2f8a3b0dc43341380672c7f0741a
Jun Shao <mr.shaojun@gmail.com>
2020-11-18 11:01:43 +1100

    call through PayPal subscription (recurring payment)

--- evc-web/src/def/subscriptionDef.js
+++ evc-web/src/def/subscriptionDef.js
@@ -7,7 +7,8 @@ export const subscriptionDef = [
     period: 'forever',
     price: '0.00',
     icon: <GiCurvyKnife/>,
-    description: "Search history data. Forever free. Goot to start with for students and trail users."
+    description: "Search history data. Forever free. Goot to start with for students and trail users.",
+    payPalPlanId: null,
   },
   {
     key: 'single',
@@ -15,7 +16,8 @@ export const subscriptionDef = [
     period: 'per month',
     price: '9.99',
     icon: <GiFireAxe/>,
-    description: "Tracking one stock. Concentrating to single stock with the updated recommendations."
+    description: "Tracking one stock. Concentrating to single stock with the updated recommendations.",
+    payPalPlanId: 'P-10L35261CU191435LL62FQTA'
   },
   {
     key: 'unlimited_month',
@@ -23,7 +25,8 @@ export const subscriptionDef = [
     period: 'per month',
     price: '39.99',
     icon: <GiSawedOffShotgun/>,
-    description: "Tracking unlimited stocks per month. Best for professtional investor as a trial."
+    description: "Tracking unlimited stocks per month. Best for professtional investor as a trial.",
+    payPalPlanId: 'P-8UX22413K5636661SL62FR6A'
   },
   {
     key: 'unlimited_quarter',
@@ -31,6 +34,7 @@ export const subscriptionDef = [
     period: 'per quarter',
     price: '109.99',
     icon: <GiPirateCannon/>,
-    description: "Tracking unlimited stocks per quater. Money saver for porfessinal investors."
+    description: "Tracking unlimited stocks per quater. Money saver for porfessinal investors.",
+    payPalPlanId: 'P-1B9632838L515502EL62FSOY'
   }
 ];

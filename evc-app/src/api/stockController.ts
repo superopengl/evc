d6f36a9a9951e79f60b1e62d7a8fb76226db9afc
Jun Shao <mr.shaojun@gmail.com>
2020-11-30 15:34:20 +1100

    fix profile

--- evc-app/src/api/stockController.ts
+++ evc-app/src/api/stockController.ts
@@ -7,7 +7,6 @@ import { assert, assertRole } from '../utils/assert';
 import { handlerWrapper } from '../utils/asyncHandler';
 import { getUtcNow } from '../utils/getUtcNow';
 import { guessDisplayNameFromFields } from '../utils/guessDisplayNameFromFields';
-import { StockHistory } from '../entity/StockHistory';
 import * as moment from 'moment';
 import { StockSearch } from '../entity/StockSearch';
 import { logError } from '../utils/logger';
@@ -79,17 +78,17 @@ export const searchSingleStock = handlerWrapper(async (req, res) => {
 export const getStockHistory = handlerWrapper(async (req, res) => {
   const symbol = req.params.symbol.toUpperCase();
 
-  const list = await getRepository(StockHistory).find({
-    where: {
-      symbol
-    },
-    order: {
-      createdAt: 'DESC'
-    }
-  });
-  assert(list.length, 404);
+  // const list = await getRepository(StockHistory).find({
+  //   where: {
+  //     symbol
+  //   },
+  //   order: {
+  //     createdAt: 'DESC'
+  //   }
+  // });
+  // assert(list.length, 404);
 
-  res.json(list);
+  res.json();
 });
 
 export const getWatchList = handlerWrapper(async (req, res) => {

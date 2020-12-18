6b5d2fd9609044356d55f1414fc2ab4a51d9153e
Jun Shao <mr.shaojun@gmail.com>
2020-12-19 08:53:38 +1100

    web can accept sse price event on StockClientPanel

--- evc-web/src/components/StockQuotePanel.js
+++ evc-web/src/components/StockQuotePanel.js
@@ -10,6 +10,10 @@ import { IconContext } from "react-icons";
 import { getStockQuote } from 'services/stockService';
 import { TimeAgo } from 'components/TimeAgo';
 import { Loading } from './Loading';
+import { GlobalContext } from 'contexts/GlobalContext';
+import { filter } from 'rxjs/operators';
+import * as moment from 'moment-timezone';
+
 const { Paragraph, Text, Title } = Typography;
 
 
@@ -18,8 +22,11 @@ const StockQuotePanel = (props) => {
   const { symbol } = props;
   const [previousPrice, setPreviousPrice] = React.useState();
   const [currentPrice, setCurrentPrice] = React.useState();
+  const [updateTime, setUpdateTime] = React.useState();
   const [quote, setQuote] = React.useState({});
+  const [priceEvent, setPriceEvent] = React.useState();
   const [loading, setLoading] = React.useState(true);
+  const context = React.useContext(GlobalContext);
 
   const loadData = async () => {
     try {
@@ -28,37 +35,61 @@ const StockQuotePanel = (props) => {
       setQuote(quote);
       setCurrentPrice(quote.latestPrice);
       setPreviousPrice(quote.latestPrice - quote.change);
+      setUpdateTime(quote.latestUpdate);
     } finally {
       setLoading(false);
     }
   }
 
   React.useEffect(() => {
+    if (priceEvent) {
+      const { price, time } = priceEvent;
+      setPreviousPrice(currentPrice);
+      setCurrentPrice(price);
+      setUpdateTime(time);
+    }
+  }, [priceEvent]);
+
+  const subscribePriceEvent = () => {
+    const subscription = context.event$.pipe(
+      filter(e => e.type === 'price'),
+      filter(e => e.data?.symbol === symbol)
+    ).subscribe(e => {
+      setPriceEvent(e.data);
+    });
+    return subscription;
+  }
+
+  React.useEffect(() => {
     loadData();
+    const subscription = subscribePriceEvent();
+    return () => {
+      subscription.unsubscribe();
+    }
   }, []);
 
   const getTrendComponent = (base, target) => {
-    if(base === target) {
+    if (base === target) {
       return <Text type="secondary">0 (0%)</Text>
     }
-    const symbol = target >= base  ? '+' : '-';
+    const symbol = target >= base ? '+' : '-';
     const type = target >= base ? 'success' : 'danger';
     const diffNumber = Math.abs(target - base);
     const diffPercentage = (diffNumber / target * 100).toFixed(2);
     return <Text type={type}><small>{symbol}{diffNumber.toFixed(2)} ({symbol}{diffPercentage}%)</small></Text>
   }
 
-  if(loading) {
-    return  <Loading />
+  if (loading) {
+    return <Loading />
   }
 
   return (
-      <Space size="large" style={{ alignItems: 'flex-end' }}>
-        <div>
-          <Text style={{ fontSize: 30 }} strong>{currentPrice} {getTrendComponent(previousPrice, currentPrice)}</Text>
-          <div><Text type="secondary"><small>Price At: 7:48AM EST</small></Text></div>
-        </div>
-        <div>
+    <Space size="large" style={{ alignItems: 'flex-end' }}>
+      <div>
+        <Text style={{ fontSize: 30 }} strong>{currentPrice} {getTrendComponent(previousPrice, currentPrice)}</Text>
+        <div><Text type="secondary"><small>Price At: {moment(updateTime).format('h:mm a')} EST</small></Text></div>
+      </div>
+      <div>
           <Text style={{ fontSize: 20 }} strong>{quote.open} {getTrendComponent(quote.open, currentPrice)}</Text>
           <div><Text type="secondary"><small>pre market</small></Text></div>
         </div>
@@ -66,7 +97,7 @@ const StockQuotePanel = (props) => {
           <Text style={{ fontSize: 20 }} strong>{quote.close} {getTrendComponent(quote.close, currentPrice)}</Text>
           <div><Text type="secondary"><small>post market</small></Text></div>
         </div>
-      </Space>
+    </Space>
   );
 };
 

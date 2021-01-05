9adab5ec7b773dc0824062baa218a09e69930098
Jun Shao <mr.shaojun@gmail.com>
2021-01-05 21:52:40 +1100

    remove unused code

--- evc-app/src/api/eventController.ts
+++ evc-app/src/api/eventController.ts
@@ -1,21 +1,7 @@
 
-import { getManager, getRepository, Like } from 'typeorm';
-import { v4 as uuidv4 } from 'uuid';
-import { Stock } from '../entity/Stock';
-import { User } from '../entity/User';
-import { assert, assertRole } from '../utils/assert';
+import { assertRole } from '../utils/assert';
 import { handlerWrapper } from '../utils/asyncHandler';
-import { getUtcNow } from '../utils/getUtcNow';
-import * as moment from 'moment';
-import { StockSearch } from '../entity/StockSearch';
-import { logError } from '../utils/logger';
-import * as geoip from 'geoip-lite';
-import * as uaParser from 'ua-parser-js';
-import { getCache, setCache } from '../utils/cache';
-import { Subscription } from '../entity/Subscription';
-import { Subject } from 'rxjs';
-import { RedisRealtimePricePubService, RedisRealtimePriceSubService, RedisSubService } from '../services/RedisPubSubService';
-import { redisCache } from '../services/redisCache';
+import { RedisRealtimePricePubService, RedisRealtimePriceSubService } from '../services/RedisPubSubService';
 
 const isProd = process.env.NODE_ENV === 'prod';
 const subscriber = new RedisRealtimePriceSubService();
--- evc-app/src/api/stripeController.ts
+++ evc-app/src/api/stripeController.ts
@@ -1,33 +1,14 @@
 
-import { getManager, getRepository, Like } from 'typeorm';
-import { v4 as uuidv4 } from 'uuid';
-import { Stock } from '../entity/Stock';
-import { User } from '../entity/User';
-import { assert, assertRole } from '../utils/assert';
+import { assertRole } from '../utils/assert';
 import { handlerWrapper } from '../utils/asyncHandler';
-import { getUtcNow } from '../utils/getUtcNow';
-import * as moment from 'moment';
-import { StockSearch } from '../entity/StockSearch';
-import { logError } from '../utils/logger';
-import * as geoip from 'geoip-lite';
-import * as uaParser from 'ua-parser-js';
-import { getCache, setCache } from '../utils/cache';
-import { Subscription } from '../entity/Subscription';
-import { Subject } from 'rxjs';
-import { RedisRealtimePricePubService, RedisRealtimePriceSubService, RedisSubService } from '../services/RedisPubSubService';
-import { redisCache } from '../services/redisCache';
 import { createStripeCheckoutSession, parseStripeWebhookEvent } from '../services/stripeService';
 
-const isProd = process.env.NODE_ENV === 'prod';
-const subscriber = new RedisRealtimePriceSubService();
-const publisher = new RedisRealtimePricePubService();
 
 export const webhookStripe = async (req, res) => {
   const event = await parseStripeWebhookEvent(req);
 
   switch (event.type) {
     case 'checkout.session.completed':
-      const session = event.data.object;
       break;
     default:
   }

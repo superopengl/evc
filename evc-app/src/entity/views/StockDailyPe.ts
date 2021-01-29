c6984415d942c747120c142c9d87c21c3f24f475
Jun Shao <mr.shaojun@gmail.com>
2021-01-29 13:53:31 +1100

    exclude those one ttmEps = 0

--- evc-app/src/entity/views/StockDailyPe.ts
+++ evc-app/src/entity/views/StockDailyPe.ts
@@ -6,26 +6,35 @@ import { StockClose } from '../StockClose';
   materialized: true,
   expression: (connection: Connection) => connection
     .createQueryBuilder()
-    .from(q => q.from(StockEps, 'eps')
-      .innerJoin(q => q.from(StockClose, 'sc'), 'sc', `sc.symbol = eps.symbol`)
-      .where(`eps."reportDate" <= sc.date`)
+    .from(q => q
+      .from(q => q.from(StockEps, 'eps')
+        .innerJoin(q => q.from(StockClose, 'sc'), 'sc', `sc.symbol = eps.symbol`)
+        .where(`eps."reportDate" <= sc.date`)
+        .select([
+          `sc.symbol`,
+          `sc.date`,
+          `sc.close`,
+          `eps.value as "epsValue"`,
+          `rank() over (partition by sc.symbol, sc.date order by eps."reportDate" desc)`
+        ]),
+        'x')
       .select([
-        `sc.symbol`,
-        `sc.date`,
-        `sc.close`,
-        `eps.value as "epsValue"`,
-        `rank() over (partition by sc.symbol, sc.date order by eps."reportDate" desc)`
-      ]),
-      'x')
+        'x.symbol',
+        'x.date',
+        'x.close',
+        'sum("epsValue") as "ttmEps"',
+      ])
+      .where('x.rank <= 4')
+      .groupBy(`x.symbol, x.date, x.close`),
+      'm')
+    .where(`m."ttmEps" > 0`)
     .select([
-      'x.symbol',
-      'x.date',
-      'x.close',
-      'sum("epsValue") as "ttmEps"',
-      `x.close / sum("epsValue") as pe`
+      'm.symbol',
+      'm.date',
+      'm.close',
+      `m."ttmEps"`,
+      `m.close / m."ttmEps" as pe`,
     ])
-    .where('x.rank <= 4')
-    .groupBy(`x.symbol, x.date, x.close`)
 })
 export class StockDailyPe {
   @ViewColumn()

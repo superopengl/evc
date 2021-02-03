7e2d9723df453afb4ff1e834fcb067eb2c581eaa
Jun Shao <mr.shaojun@gmail.com>
2021-02-03 15:54:41 +1100

    stock special fair value table

---
+++ evc-app/src/entity/StockSpecialFairValue.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, Index, PrimaryGeneratedColumn, CreateDateColumn, DeleteDateColumn } from 'typeorm';
+import { ColumnNumericTransformer } from '../utils/ColumnNumericTransformer';
+
+
+@Entity()
+@Index(['symbol', 'createdAt'])
+export class StockSpecialFairValue {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date;
+  
+  @Column()
+  symbol: string;
+
+  @Column({ type: 'date' })
+  date?: string;
+
+  @Column('uuid')
+  author: string;
+
+  @Column('decimal', { transformer: new ColumnNumericTransformer(), nullable: false })
+  fairValueLo: number;
+
+  @Column('decimal', { transformer: new ColumnNumericTransformer(), nullable: false })
+  fairValueHi: number;
+
+  @Column({default: false})
+  published: boolean;
+}

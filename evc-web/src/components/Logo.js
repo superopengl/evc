1a573a46b4d33e8f0097d92b95104d36a38cc8ab
Jun Shao <mr.shaojun@gmail.com>
2021-03-11 22:57:10 +1100

    logo refine

--- evc-web/src/components/Logo.js
+++ evc-web/src/components/Logo.js
@@ -4,5 +4,16 @@ import { Link } from 'react-router-dom';
 
 export const Logo = () =>
   <Link to="/">
-    <img alt="EasyValueCheck logo" src="/images/header-logo.png" width="auto" height="32"></img>
+    <div style={{
+      marginLeft: 'auto',
+      marginRight:'auto',
+      backgroundImage: `url('/android-chrome-192x192.png')`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'contain',
+      backgroundPosition: 'center',
+      width: 80,
+      height: 80,
+      borderRadius: 4,
+      cursor: 'pointer',
+    }}></div>
   </Link>

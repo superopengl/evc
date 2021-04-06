a23150315e117a5c368780a93502a26c30c1df3a
Jun Shao <mr.shaojun@gmail.com>
2021-04-06 11:16:24 +1000

    daily-eps to scrub support/resistance and watchlist emails

--- evc-app/endpoints/feed-eps.ts
+++ evc-app/endpoints/feed-eps.ts
@@ -1,4 +1,4 @@
-import { getRepository } from 'typeorm';
+import { getManager, getRepository } from 'typeorm';
 import { refreshMaterializedView } from '../src/db';
 import { start } from './jobStarter';
 import { Stock } from '../src/entity/Stock';
@@ -7,10 +7,78 @@ import { executeWithDataEvents } from '../src/services/dataLogService';
 import * as sleep from 'sleep-promise';
 import errorToJson from 'error-to-json';
 import { isUSMarketOpen } from '../src/services/iexService';
+import { StockResistance } from '../src/entity/StockResistance';
+import { StockSupport } from '../src/entity/StockSupport';
+import { StockDeprecateResistance } from '../src/entity/views/StockDeprecateResistance';
+import { StockDeprecateSupport } from '../src/entity/views/StockDeprecateSupport';
+import { CoreDataPreviousSnapshot } from '../src/entity/CoreDataPreviousSnapshot';
+import { CoreDataLatestSnapshot } from '../src/entity/views/CoreDataLatestSnapshot';
+import { CoreDataWatchlistEmailTask } from '../src/entity/views/CoreDataWatchlistEmailTask';
+import { sendEmail } from '../src/services/emailService';
+import { EmailTemplateType } from '../src/types/EmailTemplateType';
 
 const JOB_NAME = 'feed-eps';
 
 const MAX_CALL_TIMES_PER_MINUTE = 50;
+
+async function scrubSupports() {
+  const list = await getRepository(StockDeprecateSupport).find();
+
+  // Delete supports
+  const ids = list.map(x => x.supportId);
+  const result = await getManager()
+    .createQueryBuilder()
+    .delete()
+    .from(StockSupport)
+    .whereInIds(ids)
+    .execute();
+}
+
+async function scrubResistances() {
+  const list = await getRepository(StockDeprecateResistance).find();
+
+  // Delete resistance
+  const ids = list.map(x => x.resistanceId);
+  const result = await getManager()
+    .createQueryBuilder()
+    .delete()
+    .from(StockResistance)
+    .whereInIds(ids)
+    .execute();
+}
+
+async function sendCoreDataChangedEmails() {
+  const list = await getRepository(CoreDataWatchlistEmailTask).find();
+
+  for (const item of list) {
+    await sendEmailByRow(item);
+  }
+}
+
+async function sendEmailByRow(item: CoreDataWatchlistEmailTask) {
+  const { email, givenName, surname, symbol } = item;
+  const name = `${givenName || ''} ${surname || ''}`.trim() || 'Client';
+  await sendEmail({
+    to: email,
+    template: EmailTemplateType.WatchlistCoreDataChangedEmail,
+    shouldBcc: false,
+    vars: {
+      toWhom: name,
+      symbol,
+    }
+  }, false);
+}
+
+async function promoteLatestSnapshotToPreviousSnapshot() {
+  const { tableName: fromTableName, schema: fromSchema } = getRepository(CoreDataLatestSnapshot).metadata;
+  const { tableName: toTableName, schema: toSchema } = getRepository(CoreDataPreviousSnapshot).metadata;
+
+  await getManager().transaction(async m => {
+    await m.delete(CoreDataPreviousSnapshot, {});
+    const sql = `INSERT INTO "${toSchema}"."${toTableName}" SELECT * FROM "${fromSchema}"."${fromTableName}"`;
+    await m.query(sql);
+  });
+}
 
 start(JOB_NAME, async () => {
   const sleepTime = 60 * 1000 / MAX_CALL_TIMES_PER_MINUTE;
@@ -41,6 +109,14 @@ start(JOB_NAME, async () => {
   }
 
   await executeWithDataEvents('refresh materialized views', JOB_NAME, refreshMaterializedView);
+
+  // Scrub supports and resistance
+  await scrubSupports();
+  await scrubResistances();
+
+  // Send watchlist emails if core data change detected.
+  await sendCoreDataChangedEmails();
+  await promoteLatestSnapshotToPreviousSnapshot();
 
   for (const err of failed) {
     console.error(err);

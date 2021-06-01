c55b83efd5b39a616273f723d7a3e15b7a8e8492
Jun Shao <mr.shaojun@gmail.com>
2021-06-01 18:42:35 +1000

    refine Chinese payment guideline

--- evc-web/src/pages/ChineseUserPaymentGuidePage.js
+++ evc-web/src/pages/ChineseUserPaymentGuidePage.js
@@ -24,19 +24,21 @@ h3 {
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 3px 10px 0 rgba(0, 0, 0, 0.1);
 }
 `;
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 const ChineseUserPaymentGuidePage = () => (
   <ContainerStyled>
     <div style={{ width: '100%', textAlign: 'center', marginBottom: '2rem' }}><Logo /></div>
-    <Title style={{ textAlign: 'center' }}>中国用户支付指南</Title>
-    <Alert description="信用卡和带有“银联”标志的储蓄卡，均可付款，流程如下" showIcon  style={{marginBottom: 30}}/>
-    <Title level={3}>如果您持有“银联”标志的信用卡或储蓄卡，请点击“PayPal”</Title>
+    <Title style={{ textAlign: 'center', marginBottom: 30 }}>中国用户支付指南</Title>
+    <Paragraph strong>
+      信用卡和带有“银联”标志的储蓄卡，均可付款，流程如下：
+    </Paragraph>
+    <Title level={5}>1. 如果您持有“银联”标志的信用卡或储蓄卡，请点击“PayPal”。</Title>
     <p><Image width={400} src="/images/cn_guide/3.png" /></p>
     <p>在弹出的页面中，选择 “Pay with a Bank Account or Credit Card”。<br /><Image width={400} src="/images/cn_guide/4.png" /></p>
     <p>在详细页面中，选择 “国家/地区”（Country/Region）下拉选择“中国”（China）。<br /><Image width={400} src="/images/cn_guide/5.png" /></p>
     <p>银联信用卡，请填写卡号、有效期限、CSC（银行卡背面最后三位数）。银联储蓄卡，只需要填写卡号。<br /><Image width={400} src="/images/cn_guide/6.png" /></p>
     <p>最后填写，姓、名、账单地址、联系信息等（必填项），选择“不，我现在不需要账户” ，点击“立即付款”。<br /><Image width={400} src="/images/cn_guide/7.png" /></p>
-    <Title level={3}>如果您持有Visa、MasterCard信用卡，请点击“信用卡支付”（Pay By Card）</Title>
+    <Title level={5}>2. 如果您持有Visa、MasterCard信用卡，请点击“信用卡支付”（Pay By Card）。</Title>
     <p><Image width={400} src="/images/cn_guide/1.png" /></p>
     <p>填写卡号、有效期、CVC安全码（卡片背面最后三位数），点击“信用卡支付”。<br /><Image width={400} src="/images/cn_guide/2.png" /></p>
 

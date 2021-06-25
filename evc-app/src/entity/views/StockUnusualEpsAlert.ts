bd99a2194f384d506481416b814a28f06d4edb0b
Jun Shao <mr.shaojun@gmail.com>
2021-06-26 01:27:12 +1000

    unusual eps alert view

---
+++ evc-app/src/entity/views/StockUnusualEpsAlert.ts
@@ -0,0 +1,49 @@
+import { ViewEntity, Connection, ViewColumn } from 'typeorm';
+import { StockEps } from '../StockEps';
+
+@ViewEntity({
+  expression: (connection: Connection) => connection
+    .createQueryBuilder()
+    .from(q => q
+      .from(StockEps, 'x')
+      .select([
+        `symbol`,
+        `"reportDate"`,
+        `value`,
+        `rank() over (partition by symbol order by "reportDate") as rank`
+      ])
+      , 'latter')
+    .innerJoin(q => q
+      .from(StockEps, 'x')
+      .select([
+        `symbol`,
+        `"reportDate"`,
+        `value`,
+        `rank() over (partition by symbol order by "reportDate") as rank`
+      ])
+      , 'former', `latter.symbol = former.symbol AND latter.rank = former.rank + 1 AND latter.value = former.value`)
+    .where(`latter."reportDate" - former."reportDate" < 80`)
+    .select([
+      `latter.symbol as symbol`,
+      `former."reportDate" as "reportDateFormer"`,
+      `latter."reportDate" as "reportDateLatter"`,
+      `latter."value" as "value"`,
+      `latter."reportDate" - former."reportDate" as "span"`,
+    ])
+})
+export class StockUnusualEpsAlert {
+  @ViewColumn()
+  symbol: string;
+
+  @ViewColumn()
+  reportDateFormer: string;
+
+  @ViewColumn()
+  reportDateLatter: string;
+
+  @ViewColumn()
+  value: number;
+
+  @ViewColumn()
+  span: number;
+}

00a880e3a0e255d4a6376878820f1ba7835f10a0
Jun Shao <mr.shaojun@gmail.com>
2023-05-25 13:17:54 +1100

    temp

--- evc-web/src/components/StockTagSelect.js
+++ evc-web/src/components/StockTagSelect.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Select } from 'antd';
+import { Typography } from 'antd';
 import * as tinycolor from 'tinycolor2';
-import { listStockTags } from 'services/stockTagService';
+import { listStockTags, saveStockTag } from 'services/stockTagService';
 import StockTag from './StockTag';
 import styled from 'styled-components';
+import CreatableSelect, { makeCreatableSelect } from 'react-select/creatable';
+import Select, { components } from 'react-select';
+import chroma from 'chroma-js';
+import { v4 as uuidv4 } from 'uuid';
 
 const { Text } = Typography;
 
@@ -25,20 +29,92 @@ const SelectStyled = styled(Select)`
 }
 `;
 
+const MultiValueLabel = props => {
+  if (!props.data.color) {
+    debugger;
+  }
+  return (
+    <StockTag color={props.data.color}>{props.data.label}</StockTag>
+    // <components.MultiValueLabel {...props} />
+  );
+};
+
+const colourStyles = {
+  // control: styles => ({ ...styles, backgroundColor: 'white' }),
+  // option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+  //   const color = chroma(data.color);
+  //   return {
+  //     ...styles,
+  //     backgroundColor: isDisabled
+  //       ? null
+  //       : isSelected
+  //         ? data.color
+  //         : isFocused
+  //           ? color.alpha(0.1).css()
+  //           : null,
+  //     color: isDisabled
+  //       ? '#ccc'
+  //       : isSelected
+  //         ? chroma.contrast(color, 'white') > 2
+  //           ? 'white'
+  //           : 'black'
+  //         : data.color,
+  //     cursor: isDisabled ? 'not-allowed' : 'default',
+
+  //     ':active': {
+  //       ...styles[':active'],
+  //       backgroundColor: !isDisabled && (isSelected ? data.color : color.alpha(0.3).css()),
+  //     },
+  //   };
+  // },
+  multiValue: (styles, { data }) => {
+    return {
+      ...styles,
+      width: '100%',
+      backgroundColor: 'none',
+      // backgroundColor: data.color,
+    };
+  },
+  // multiValueLabel: (styles, { data }) => ({
+  //   ...styles,
+  //   width: '100%',
+  //   color: data.color,
+  // }),
+  // multiValueRemove: (styles, { data }) => {
+  //   const color = chroma(data.color);
+  //   return {
+  //     ...styles,
+  //     color: data.color,
+  //     ':hover': {
+  //       backgroundColor: color.alpha(0.1).css(),
+  //       color: 'white',
+  //     },
+  //   }
+  // },
+};
+
 const StockTagSelect = (props) => {
 
   const { value, onChange } = props;
   const [tagList, setTagList] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
-  const [selectedTags, setSelectedTags] = React.useState(value);
+  const [defaultValue, setDefaultValue] = React.useState([]);
+  const [options, setOptions] = React.useState([]);
 
   const loadEntity = async () => {
     try {
       setLoading(true);
       const allTags = await listStockTags();
       setTagList(allTags);
-      setSelectedTags(value.map(id => allTags.find(t => t.id === id)).filter(x => x))
-      debugger;
+      const options = tagList
+        // .filter(t => !selectedTags.includes(t.id))
+        // .filter(t => !defaultValue.some(x => x.key === t.id))
+        .map(x => ({
+          value: x.id,
+          label: x.name,
+          color: x.color
+        }));
+      setOptions(options);
     } finally {
       setLoading(false);
     }
@@ -48,31 +124,78 @@ const StockTagSelect = (props) => {
     loadEntity();
   }, []);
 
-  const options = tagList
-  // .filter(t => !selectedTags.includes(t.id))
-  .filter(t => !selectedTags.some(x => x.key === t.id))
-  .map(x => x);
 
   const handleChange = selected => {
-    setSelectedTags(selected);
+    setDefaultValue(selected);
     onChange(selected.map(s => s.key));
   }
 
-  return (
-    <SelectStyled
-      mode="multiple"
-      allowClear={false}
-      style={{ minWidth: 200 }}
-      onChange={handleChange}
-      disabled={loading}
-      value={selectedTags}
-      labelInValue
-    >
-      {options.map((t, i) => <Select.Option key={t.id} value={t.id}>
-        <StockTag color={t.color}>{t.name}</StockTag>
-      </Select.Option>)}
-    </SelectStyled>
-  );
+  const handleChangeNew = async (newValue, actionMeta) => {
+    debugger;
+    switch (actionMeta.action) {
+      case 'create-option':
+        break;
+      case 'select-option':
+        break;
+      case 'remove-value':
+        break;
+      default:
+        throw new Error(`Unsupported action ${actionMeta.action}`);
+    }
+  }
+
+  const handleCreateNew = async (newTagName) => {
+    debugger;
+    const tagId = uuidv4();
+    const newTag = {
+      id: tagId,
+      name: newTagName,
+      color: tinycolor.random().toHexString()
+    };
+    const newOption = {
+      label: newTagName,
+      value: tagId,
+      color: newTag.color
+    };
+    try {
+      setLoading(true);
+      await saveStockTag(newTag);
+      setOptions([...options, newOption]);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return <CreatableSelect
+    isMulti
+    closeMenuOnSelect={false}
+    components={{ MultiValueLabel }}
+    isClearable={false}
+    isSearchable={true}
+    isLoading={loading}
+    onChange={handleChangeNew}
+    onCreateOption={handleCreateNew}
+
+    defaultValue={defaultValue}
+    styles={colourStyles}
+    options={options}
+  />
+
+  // return (
+  //   <SelectStyled
+  //     mode="multiple"
+  //     allowClear={false}
+  //     style={{ minWidth: 200 }}
+  //     onChange={handleChange}
+  //     disabled={loading}
+  //     value={selectedTags}
+  //     labelInValue
+  //   >
+  //     {options.map((t, i) => <Select.Option key={t.id} value={t.id}>
+  //       <StockTag color={t.color}>{t.name}</StockTag>
+  //     </Select.Option>)}
+  //   </SelectStyled>
+  // );
 };
 
 StockTagSelect.propTypes = {

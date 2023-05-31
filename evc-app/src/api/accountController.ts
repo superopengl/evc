7ebacad8978cd94731389ebb5d5edc8ac4c87334
Jun Shao <mr.shaojun@gmail.com>
2023-05-31 15:34:20 +1100

    fix profile

--- evc-app/src/api/accountController.ts
+++ evc-app/src/api/accountController.ts
@@ -7,7 +7,6 @@ import { assert, assertRole } from '../utils/assert';
 import { handlerWrapper } from '../utils/asyncHandler';
 import { getUtcNow } from '../utils/getUtcNow';
 import { guessDisplayNameFromFields } from '../utils/guessDisplayNameFromFields';
-import { StockHistory } from '../entity/StockHistory';
 import * as moment from 'moment';
 import { StockSearch } from '../entity/StockSearch';
 import { logError } from '../utils/logger';

24d6345268a7460fb01075802477cbcc846f8dae
Jun Shao <mr.shaojun@gmail.com>
2023-06-04 19:06:35 +1100

    e2e run through card payment with paymentIntent

--- evc-web/src/components/StripeCardPaymentWidget.js
+++ evc-web/src/components/StripeCardPaymentWidget.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
-import { Typography, Button } from 'antd';
+import { Space, Button } from 'antd';
 import { notify } from 'util/notify';
 import PropTypes from 'prop-types';
 import * as _ from 'lodash';
-import { commitSubscription, fetchStripeCheckoutSession, confirmSubscriptionPayment } from 'services/subscriptionService';
-import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js';
+import { confirmSubscriptionPayment } from 'services/subscriptionService';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const StripeCardPaymentWidget = (props) => {
 
   const { onProvision, onOk } = props;
-  const [loading, setLoading] = React.useState(false);
+  const [loading] = React.useState(false);
+  const [inputComplete, setInputComplete] = React.useState(false);
   const stripe = useStripe();
   const elements = useElements();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!stripe || !elements) {
+    if (!stripe || !elements || !inputComplete) {
       return;
     }
 
@@ -47,21 +47,46 @@ const StripeCardPaymentWidget = (props) => {
 
   };
 
+  const handleCardInfoChange = (element) => {
+    setInputComplete(element.complete)
+  }
+
   return (
     <form onSubmit={handleSubmit}>
-      <CardElement />
-      <Button type="primary" htmlType="submit" block disabled={loading || !stripe}>Pay by Card</Button>
+      <Space direction="vertical" style={{ width: '100%' }} size="large">
+
+        <CardElement
+          onChange={handleCardInfoChange}
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px',
+                // color: '#424770',
+                '::placeholder': {
+                  color: 'rgba(0,0,0,0.2)',
+                },
+              },
+              invalid: {
+                color: '#d7183f',
+              },
+            },
+          }}
+        />
+        <Button type="primary" htmlType="submit" block disabled={loading || !stripe || !inputComplete}>Pay by Card</Button>
+      </Space>
     </form>
   )
 }
 
 
 StripeCardPaymentWidget.propTypes = {
-  onProvision: PropTypes.func,
+  onProvision: PropTypes.func.isRequired,
   onOk: PropTypes.func
 };
 
 StripeCardPaymentWidget.defaultProps = {
+  onOk: () => { }
 };
 
 export default withRouter(StripeCardPaymentWidget);

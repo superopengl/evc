bb75d78f3b3c4b8b33f02ea14cda3b63b401a11e
Jun Shao <mr.shaojun@gmail.com>
2023-07-30 17:21:36 +1100

    refactor mviews

--- evc-app/src/entity/views/StockDailyPe.ts
+++ evc-app/src/entity/views/StockDailyPe.ts
@@ -2,41 +2,32 @@ import { ViewEntity, Connection, ViewColumn } from 'typeorm';
 import { StockEps } from '../StockEps';
 import { StockClose } from '../StockClose';
 
+
 @ViewEntity({
   materialized: true,
   expression: (connection: Connection) => connection
     .createQueryBuilder()
-    .from(q => q
-      .from(q => q.from(StockEps, 'eps')
-        .innerJoin(q => q.from(StockClose, 'sc'), 'sc', `sc.symbol = eps.symbol`)
-        .where(`eps."reportDate" <= sc.date`)
-        .select([
-          `sc.symbol`,
-          `sc.date`,
-          `sc.close`,
-          `eps.value as "epsValue"`,
-          `rank() over (partition by sc.symbol, sc.date order by eps."reportDate" desc)`
-        ]),
-        'x')
+    .from(q => q.from(StockEps, 'eps')
+      .innerJoin(q => q.from(StockClose, 'sc'), 'sc', `sc.symbol = eps.symbol`)
+      .where(`eps."reportDate" <= sc.date`)
       .select([
-        'x.symbol',
-        'x.date',
-        'x.close',
-        'sum("epsValue") as "ttmEps"',
-      ])
-      .where('x.rank <= 4')
-      .groupBy(`x.symbol, x.date, x.close`),
-      'm')
-    .where(`m."ttmEps" > 0`)
+        `sc.symbol`,
+        `sc.date`,
+        `sc.close`,
+        `eps.value as "epsValue"`,
+        `rank() over (partition by sc.symbol, sc.date order by eps."reportDate" desc)`
+      ]),
+      'x')
     .select([
-      'm.symbol',
-      'm.date',
-      'm.close',
-      `m."ttmEps"`,
-      `m.close / m."ttmEps" as pe`,
+      'symbol',
+      'date',
+      'close',
+      'sum("epsValue") as "ttmEps"',
     ])
+    .where('rank <= 4')
+    .groupBy(`symbol, date, close`)
 })
-export class StockDailyPe {
+export class StockDailyPeRaw {
   @ViewColumn()
   symbol: string;
 
@@ -46,3 +37,53 @@ export class StockDailyPe {
   @ViewColumn()
   pe: number;
 }
+@ViewEntity({
+  materialized: true,
+  expression: (connection: Connection) => connection
+    .createQueryBuilder()
+    .from(StockDailyPeRaw, 'x')
+    .where(`"ttmEps" > 0`)
+    .select([
+      'symbol',
+      'date',
+      'close',
+      `"ttmEps"`,
+      `close / "ttmEps" as pe`,
+    ])
+})
+export class StockDailyPe{
+  @ViewColumn()
+  symbol: string;
+
+  @ViewColumn()
+  date: string;
+
+  @ViewColumn()
+  pe: number;
+}
+
+@ViewEntity({
+  materialized: true,
+  expression: (connection: Connection) => connection
+    .createQueryBuilder()
+    .from(StockDailyPeRaw, 'x')
+    .where(`"ttmEps" <= 0`)
+    .select([
+      'symbol',
+      'date',
+      'close',
+      `"ttmEps"`,
+      `'N/A' as pe`,
+    ])
+})
+export class StockDailyPeNa{
+  @ViewColumn()
+  symbol: string;
+
+  @ViewColumn()
+  date: string;
+
+  @ViewColumn()
+  pe: number;
+}
+

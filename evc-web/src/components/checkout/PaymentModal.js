aad8f10b6fe6d4efa4b9afdd3c4b37902812e37b
Jun Shao <mr.shaojun@gmail.com>
2023-10-12 23:32:20 +1000

    stepper payment flow

--- evc-web/src/components/checkout/PaymentModal.js
+++ evc-web/src/components/checkout/PaymentModal.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
-import { Typography, Button, Switch, Divider } from 'antd';
+import { Typography, Button, Switch, Divider, Steps } from 'antd';
 import { getAuthUser } from 'services/authService';
 import PropTypes from 'prop-types';
 import { PayPalCheckoutButton } from 'components/checkout/PayPalCheckoutButton';
 import { Alert, Modal, Space } from 'antd';
-import { AlipayCircleOutlined } from '@ant-design/icons';
+import Icon, { AlipayCircleOutlined, SettingOutlined } from '@ant-design/icons';
 import { subscriptionDef } from 'def/subscriptionDef';
 import * as _ from 'lodash';
 import MoneyAmount from '../MoneyAmount';
@@ -17,6 +17,8 @@ import StripeCardPaymentWidget from './StripeCardPaymentWidget';
 import ReactDOM from 'react-dom';
 import { GlobalContext } from 'contexts/GlobalContext';
 import { SubscriptionCard } from 'components/SubscriptionCard';
+import { FaCashRegister } from 'react-icons/fa';
+import { BsCardChecklist } from 'react-icons/bs';
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -41,6 +43,7 @@ const PaymentModal = (props) => {
   const [recurring, setRecurring] = React.useState(true);
   const [paymentDetail, setPaymentDetail] = React.useState();
   const [willUseCredit, setWillUseCredit] = React.useState(false);
+  const [currentStep, setCurrentStep] = React.useState(0);
   const context = React.useContext(GlobalContext);
 
   const fetchPaymentDetail = async (useCredit) => {
@@ -112,11 +115,65 @@ const PaymentModal = (props) => {
   //   });
   // }
 
+  const handleStepChange = current => {
+    setCurrentStep(current);
+  }
+
   const shouldShowFullCreditButton = isValidPlan && !recurring && willUseCredit && paymentDetail.additionalPay === 0;
   const shouldShowCard = isValidPlan && (paymentDetail.additionalPay > 0 || recurring);
   const shouldShowPayPal = isValidPlan && !recurring && paymentDetail.additionalPay > 0;
   const shouldShowAliPay = isValidPlan && !recurring && paymentDetail.additionalPay > 0;
   const showCreditCardCombinedRecurringMessage = recurring && willUseCredit;
+
+  const stepDef = [
+    {
+      component: <Space direction="vertical" style={{ width: '100%' }}>
+        <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text>Recurring payment? (Auto renew subscription at the end day)</Text>
+          <Switch defaultChecked={recurring} onChange={handleRecurringChange} />
+        </Space>
+        {paymentDetail?.totalCreditAmount > 0 && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text>Prefer to use credit?</Text>
+          <Switch defaultChecked={false} onChange={handleUseCreditChange} />
+        </Space>}
+        {willUseCredit && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text>Total credit:</Text>
+          {paymentDetail ? <MoneyAmount value={paymentDetail.totalCreditAmount} /> : '-'}
+        </Space>}
+        <Divider />
+        <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text>Total amount:</Text>
+          {paymentDetail ? <MoneyAmount value={paymentDetail.price} /> : '-'}
+        </Space>
+        {willUseCredit && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text>Credit deduction:</Text>
+          {paymentDetail ? <MoneyAmount value={paymentDetail.creditDeductAmount * -1} /> : '-'}
+        </Space>}
+        <Space style={{ width: '100%', justifyContent: 'space-between' }}>
+          <Text strong>Total payable amount:</Text>
+          {paymentDetail ? <MoneyAmount style={{ fontSize: '1.2rem' }} strong value={paymentDetail.additionalPay} /> : '-'}
+        </Space>
+        <Button type="primary" block style={{ marginTop: 20 }} disabled={!isValidPlan} onClick={() => handleStepChange(1)}>Next</Button>
+      </Space>
+    },
+    {
+      component: <Space direction="vertical" style={{ width: '100%' }} size="middle">
+        <Space style={{ width: '100%', justifyContent: 'space-between', marginBottom: 20 }}>
+          <Text strong>Total payable amount:</Text>
+          {paymentDetail ? <MoneyAmount style={{ fontSize: '1.2rem' }} strong value={paymentDetail.additionalPay} /> : '-'}
+        </Space>
+        {shouldShowFullCreditButton && <>
+          <Alert type="info" description="Congratulations! You have enough credit to purchase this plan without any additional pay." showIcon />
+          <FullCreditPayButton onProvision={() => handleProvisionSubscription('credit')} onCommit={handleSuccessfulPayment} />
+        </>}
+        {showCreditCardCombinedRecurringMessage && <Alert
+          type="info" description="When each plan renew happens, system will try to use your credit as much before charging your card." showIcon />}
+        {shouldShowCard && <StripeCardPaymentWidget onProvision={() => handleProvisionSubscription('card')} onCommit={handleSuccessfulPayment} />}
+        {shouldShowAliPay && <AlipayButton size="large" icon={<AlipayCircleOutlined />} block style={{ fontWeight: 800, fontStyle: 'italic' }}>Alipay</AlipayButton>}
+        {shouldShowPayPal && <PayPalCheckoutButton onProvision={() => handleProvisionSubscription('paypal')} onCommit={handleSuccessfulPayment} />}
+      </Space>
+    }
+  ];
 
   return (
     <Modal
@@ -130,7 +187,6 @@ const PaymentModal = (props) => {
       onOk={() => onCancel()}
       onCancel={() => onCancel()}
     >
-
       <Loading loading={loading} message={'In progress. Please do not close the window.'}>
         <Space direction="vertical" style={{ width: '100%' }} >
           <Space style={{ width: '100%', justifyContent: 'space-between' }}>
@@ -140,44 +196,13 @@ const PaymentModal = (props) => {
           <div style={{ display: 'flex' }}>
             {newPlanDef.description}
           </div>
-          <Divider />
-          <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text>Recurring payment? (Auto renew subscription at the end day)</Text>
-            <Switch defaultChecked={recurring} onChange={handleRecurringChange} />
-          </Space>
-          {paymentDetail?.totalCreditAmount > 0 && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text>Prefer to use credit?</Text>
-            <Switch defaultChecked={false} onChange={handleUseCreditChange} />
-          </Space>}
-          {willUseCredit && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text>Total credit:</Text>
-            {paymentDetail ? <MoneyAmount value={paymentDetail.totalCreditAmount} /> : '-'}
-          </Space>}
-          <Divider />
-          <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text>Total amount:</Text>
-            {paymentDetail ? <MoneyAmount value={paymentDetail.price} /> : '-'}
-          </Space>
-          {willUseCredit && <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text>Credit deduction:</Text>
-            {paymentDetail ? <MoneyAmount value={paymentDetail.creditDeductAmount * -1} /> : '-'}
-          </Space>}
-          <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-            <Text strong>Total payable amount:</Text>
-            {paymentDetail ? <MoneyAmount style={{ fontSize: '1.2rem' }} strong value={paymentDetail.additionalPay} /> : '-'}
-          </Space>
-          {isValidPlan && <Divider />}
-          {isValidPlan && <Space direction="vertical" style={{ width: '100%' }} size="middle">
-            {shouldShowFullCreditButton && <>
-              <Alert type="info" description="Congratulations! You have enough credit to purchase this plan without any additional pay." showIcon />
-              <FullCreditPayButton onProvision={() => handleProvisionSubscription('credit')} onCommit={handleSuccessfulPayment} />
-            </>}
-            {showCreditCardCombinedRecurringMessage && <Alert
-              type="info" description="When each plan renew happens, system will try to use your credit as much before charging your card." showIcon />}
-            {shouldShowCard && <StripeCardPaymentWidget onProvision={() => handleProvisionSubscription('card')} onCommit={handleSuccessfulPayment} />}
-            {shouldShowAliPay && <AlipayButton size="large" icon={<AlipayCircleOutlined />} block style={{ fontWeight: 800, fontStyle: 'italic' }}>Alipay</AlipayButton>}
-            {shouldShowPayPal && <PayPalCheckoutButton onProvision={() => handleProvisionSubscription('paypal')} onCommit={handleSuccessfulPayment} />}
-          </Space>}
+          <Steps current={currentStep} onChange={handleStepChange} style={{ margin: '30px 0 20px' }}>
+            <Steps.Step title="Options" icon={<Icon component={() => <BsCardChecklist />} />} />
+            <Steps.Step title="Checkout" icon={<Icon component={() => <FaCashRegister />} />} />
+          </Steps>
+          <div style={{ width: '100%' }}>
+            {stepDef[currentStep].component}
+          </div>
         </Space>
       </Loading>
 

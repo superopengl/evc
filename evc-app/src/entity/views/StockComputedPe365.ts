ebb727a0bd47068a2ad347a618b5bce64e71a829
Jun Shao <mr.shaojun@gmail.com>
2023-11-16 09:54:19 +1100

    view of pe365

--- evc-app/src/entity/views/StockComputedPe365.ts
+++ evc-app/src/entity/views/StockComputedPe365.ts
@@ -8,50 +8,41 @@ import { StockDailyAdvancedStat } from '../StockDailyAdvancedStat';
   materialized: true,
   expression: (connection: Connection) => connection
     .createQueryBuilder()
-    .from(q => q.from(StockComputedPe90, 'pe')
-      .innerJoin(StockComputedPe90, 'back', 'pe.symbol = back.symbol')
-      .where('back.date between pe."date" - 365 and pe."date"')
-      // .andWhere(`exists(select 1 from evc.stock_all_computed_pe sdp where sdp.date = pe."date" - 90)`)
-      .groupBy('pe.symbol')
-      .addGroupBy('pe.date')
-      .addGroupBy('pe.close')
-      .addGroupBy('pe."ttmEps"')
-      .addGroupBy('pe.pe')
-      .addGroupBy('pe."pe90Avg"')
-      .addGroupBy('pe."pe90StdDev"')
-      .addGroupBy('pe."fairValueLo"')
-      .addGroupBy('pe."fairValueHi"')
+    .from(StockComputedPe90, 'pe')
+    .leftJoin(q => q.from(StockDailyAdvancedStat, 'a')
+      .innerJoin(StockDailyAdvancedStat, 'back', 'back.symbol = a.symbol')
+      .where('back."date" between a."date" - 365 AND a."date"')
+      .groupBy('a.symbol')
+      .addGroupBy('a."date"')
+      .addGroupBy('a."beta"')
+      .addGroupBy('a."peRatio"')
+      .addGroupBy('a."pegRatio"')
       .select([
-        'pe.symbol as symbol',
-        'pe.date as date',
-        'pe.close as close',
-        'pe."ttmEps" as "ttmEps"',
-        'pe.pe as pe',
-        'pe."pe90Avg" as "pe90Avg"',
-        'pe."pe90StdDev" as "pe90StdDev"',
-        'pe."fairValueLo" as "fairValueLo"',
-        'pe."fairValueHi" as "fairValueHi"',
-        'min(back.pe) as "peYrLo"',
-        'max(back.pe) as "peYrHi"'
-      ])
-      , 'x')
-    .leftJoin(StockDailyAdvancedStat, 'a', 'x.symbol = a.symbol AND x.date = a.date')
+        'a.symbol as symbol',
+        'a."date" as "date"',
+        'a."beta" as "beta"',
+        'a."peRatio" as "peRatio"',
+        'a."pegRatio" as "pegRatio"',
+        'min(back."peRatio") as "peYrLo"',
+        'max(back."peRatio") as "peYrHi"'
+      ]), 'x'
+      , 'x.symbol = pe.symbol AND x."date" = pe."date"')
     .select([
-      'x.symbol',
-      'x.date',
-      'x.close',
-      'x."ttmEps"',
-      'x.pe',
-      'x."pe90Avg" as "pe90Avg"',
-      'x."pe90StdDev" as "pe90StdDev"',
+      'pe.symbol as symbol',
+      'pe.date as date',
+      'pe.close as close',
+      'pe."ttmEps" as "ttmEps"',
+      'pe.pe as pe',
+      'pe."pe90Avg" as "pe90Avg"',
+      'pe."pe90StdDev" as "pe90StdDev"',
+      'pe."fairValueLo" as "fairValueLo"',
+      'pe."fairValueHi" as "fairValueHi"',
+      'x.beta as beta',
+      'x."peRatio" as "peRatio"',
+      'x."pegRatio" as "pegRatio"',
       'x."peYrLo" as "peYrLo"',
       'x."peYrHi" as "peYrHi"',
-      'x."ttmEps" * (x."pe90Avg" - x."pe90StdDev") as "fairValueLo"',
-      'x."ttmEps" * (x."pe90Avg" + x."pe90StdDev") as "fairValueHi"',
-      'a.beta as beta',
-      'a."peRatio" as "peRatio"',
-      'a."pegRatio" as "pegRatio"',
-      'x."ttmEps" * (1 + x.pe / a."pegRatio") as "forwardEps"',
+      'pe."ttmEps" * (1 + x."peRatio" / x."pegRatio") as "forwardEps"',
     ])
 })
 export class StockComputedPe365 {
@@ -77,12 +68,6 @@ export class StockComputedPe365 {
   pe90StdDev: number;
 
   @ViewColumn()
-  peYrLo: number;
-
-  @ViewColumn()
-  peYrHi: number;
-
-  @ViewColumn()
   fairValueLo: number;
 
   @ViewColumn()
@@ -96,6 +81,12 @@ export class StockComputedPe365 {
 
   @ViewColumn()
   pegRatio: number;
+
+  @ViewColumn()
+  peYrLo: number;
+
+  @ViewColumn()
+  peYrHi: number;
 
   @ViewColumn()
   forwardEps: number;

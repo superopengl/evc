f9b2747c302d8b716d212b59cfbb8b10fdba8caa
Jun Shao <mr.shaojun@gmail.com>
2023-11-19 23:46:36 +1000

    calculate discount in backend

--- evc-app/src/utils/getNewSubscriptionPaymentInfo.ts
+++ evc-app/src/utils/getNewSubscriptionPaymentInfo.ts
@@ -1,13 +1,17 @@
 import { SubscriptionType } from '../types/SubscriptionType';
 import { getSubscriptionPrice } from './getSubscriptionPrice';
 import { getUserCreditBalance } from './getUserCreditBalance';
+import { getRepository } from 'typeorm';
+import { UserCommissionDiscountInformation } from '../entity/views/UserCommissionDiscountInformation';
 
 export async function getNewSubscriptionPaymentInfo(
   userId: string,
   subscriptionType: SubscriptionType,
 ) {
-  const price = getSubscriptionPrice(subscriptionType);
+  const fullPrice = getSubscriptionPrice(subscriptionType);
   const creditBalance = await getUserCreditBalance(userId);
+  const { my1stBuyDiscountPerc } = await getRepository(UserCommissionDiscountInformation).findOne(userId);
+  const price = fullPrice * (1 - (my1stBuyDiscountPerc || 0));
   const result = {
     price,
     creditBalance,

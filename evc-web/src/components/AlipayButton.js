4964b9619e26cd5f2d0e29354a67e7e1b3226f68
Jun Shao <mr.shaojun@gmail.com>
2023-11-25 16:28:36 +1000

    responsive refine

--- evc-web/src/components/AlipayButton.js
+++ evc-web/src/components/AlipayButton.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Button, Image } from 'antd';
-import { notify } from 'util/notify';
-import PropTypes from 'prop-types';
-import { useStripe, Elements } from '@stripe/react-stripe-js';
 import styled from 'styled-components';
-import Icon from '@ant-design/icons';
 
 const StyledButton = styled(Button)`
 border-color: #108fe9;

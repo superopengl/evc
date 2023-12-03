e8db6b430d2c87f178d8dbc66bd0ba35114f32ec
Jun Shao <mr.shaojun@gmail.com>
2023-12-03 13:36:08 +1000

    set fillfactor = 30

---
+++ evc-app/migration/1622777518161-AlterTableFillFactor.ts
@@ -0,0 +1,17 @@
+import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
+import { StockLastPrice } from '../src/entity/StockLastPrice';
+import { StockEps } from '../src/entity/StockEps';
+
+export class AlterTableFillFactor1622777518161 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const tables = [StockLastPrice, StockEps];
+        for (const table of tables) {
+            const { schema, tableName } = getRepository(table).metadata;
+            queryRunner.query(`ALTER TABLE "${schema}"."${tableName}" SET (fillfactor = 30)`)
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

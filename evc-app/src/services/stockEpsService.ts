49900dfc0f9fa483b1bf6292ae9d0c4dc3bf36c8
Jun Shao <mr.shaojun@gmail.com>
2023-12-05 18:59:06 +1000

    feed-eps 3 attempts for each AlphaVantage call

--- evc-app/src/services/stockEpsService.ts
+++ evc-app/src/services/stockEpsService.ts
@@ -2,6 +2,7 @@ import { getManager } from 'typeorm';
 import { StockEps } from '../entity/StockEps';
 import { getEarnings } from './alphaVantageService';
 import * as _ from 'lodash';
+import * as delay from 'delay';
 
 type StockIexEpsInfo = {
   symbol: string;
@@ -9,12 +10,44 @@ type StockIexEpsInfo = {
   value: number;
 };
 
-export const syncStockEps = async (symbol: string, howManyQuarters = 8) => {
-  const { earnings, rawResponse } = await getEarnings(symbol, howManyQuarters);
+async function getEarningsWithThreeAttempts(symbol: string, howManyQuarters: number) {
+  let earnings;
+  let rawResponse;
+
+  const attempt1 = await getEarnings(symbol, howManyQuarters);
+  earnings = attempt1.earnings;
+  rawResponse = attempt1.rawResponse;
+
   if (!earnings?.length) {
-    console.log('Nothing returned form API for ', symbol, JSON.stringify(rawResponse));
+    console.log('Attempt 1: Nothing returned form API for ', symbol, JSON.stringify(rawResponse), '. Next attempt after 300 ms.');
+    await delay(300);
+    const attempt2 = await getEarnings(symbol, howManyQuarters);
+    earnings = attempt2.earnings;
+    rawResponse = attempt2.rawResponse;
+  }
+
+  if (!earnings?.length) {
+    console.log('Attempt 2: Nothing returned form API for ', symbol, JSON.stringify(rawResponse), '. Next attempt after 1000 ms.');
+    await delay(1000);
+    const attempt3 = await getEarnings(symbol, howManyQuarters);
+    earnings = attempt3.earnings;
+    rawResponse = attempt3.rawResponse;
+  }
+
+  if (!earnings?.length) {
+    console.log('Attempt 3: Nothing returned form API for ', symbol, JSON.stringify(rawResponse));
+  }
+
+  return earnings;
+}
+
+
+export const syncStockEps = async (symbol: string, howManyQuarters = 8) => {
+  const earnings = await getEarningsWithThreeAttempts(symbol, howManyQuarters);
+  if (!earnings?.length) {
     return;
   }
+
   const infoList = _.chain(earnings)
     .map(e => {
       const data: StockIexEpsInfo = {

7e4d3079192a6fe0c58875b280ca1fa610365817
Jun Shao <mr.shaojun@gmail.com>
2024-04-25 13:16:32 +1100

    typo fix

--- evc-web/src/pages/AdminDashboard/DataSourcePage.js
+++ evc-web/src/pages/AdminDashboard/DataSourcePage.js
@@ -92,7 +92,7 @@ GOOG,1000-1100,2000
           The csv uploads for Unusual Options Activity do not handle duplicate records. Duplicated upload will cause the same data appear multiple time.
         </Paragraph>
         <Text><pre><small>
-          {`Symbol,Price,Type,Strike,"Exp Date",DTE,Bid,Midpoint,Ask,Last,Volume,"Open Int",Vol/OI,IV,Trade Date,Time
+          {`Symbol,Price,Type,Strike,"Exp Date",DTE,Bid,Midpoint,Ask,Last,Volume,"Open Int",Vol/OI,IV,Time
 CCJ,16.5,Call,35,09/17/21,172,0.36,0.37,0.38,0.38,15103,256,59,74.80%,03/29/21
 GSX,31.78,Call,40,04/16/21,18,1.8,1.99,2.18,1.85,6521,133,49.03,155.62%,03/29/21
 KTOS,27.43,Call,30,04/16/21,18,1.1,1.18,1.25,1.15,25473,575,44.3,86.68%,03/29/21

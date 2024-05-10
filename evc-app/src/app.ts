220b9ee4fd0b741eed57507e2ba5ab3c450fc96c
Jun Shao <mr.shaojun@gmail.com>
2024-05-10 19:24:44 +1100

    improve cache-control for Chrome

--- evc-app/src/app.ts
+++ evc-app/src/app.ts
@@ -104,8 +104,6 @@ export function createAppInstance() {
   app.use(authMiddleware);
   app.use((req, res, next) => {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
     next();
   });
   // app.use(passport.initialize());

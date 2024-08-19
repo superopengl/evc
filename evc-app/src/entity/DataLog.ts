606c1ea0206bc7555593f6af67f4ecadf25e58dd
Jun Shao <mr.shaojun@gmail.com>
2024-08-19 18:13:20 +1100

    add indexes

--- evc-app/src/entity/DataLog.ts
+++ evc-app/src/entity/DataLog.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, Index } from 'typeorm';
 
 @Entity()
 export class DataLog {
@@ -6,18 +6,23 @@ export class DataLog {
   id?: number;
 
   @CreateDateColumn()
+  @Index()
   createdAt?: Date;
 
   @Column()
+  @Index()
   by: string;
 
   @Column('uuid')
+  @Index()
   eventId: string;
 
   @Column()
+  @Index()
   eventType: string;
 
   @Column({ default: 'info' })
+  @Index()
   level?: string;
 
   @Column({ type: 'json', nullable: true })

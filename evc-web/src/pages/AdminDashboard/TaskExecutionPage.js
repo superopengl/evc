fe9537a6082a7b131230e68443ebc834e553641c
Jun Shao <mr.shaojun@gmail.com>
2024-08-25 20:32:26 +1000

    some attempts

--- evc-web/src/pages/AdminDashboard/TaskExecutionPage.js
+++ evc-web/src/pages/AdminDashboard/TaskExecutionPage.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { Typography } from 'antd';
 import { Bar } from '@ant-design/plots';
 import { getTaskLogChart$ } from 'services/dataService';
+import * as moment from 'moment-timezone';
 const { Title } = Typography;
 
 const span = {
@@ -17,22 +18,41 @@ const span = {
 const TaskExecutionPage = () => {
   const [list, setList] = React.useState([]);
 
+  const utcStringToEstEpochMills = (utcString) => {
+    return moment.utc(utcString).tz('America/New_York').format('D MMM');
+  }
+
+  const formatData = (data) => {
+    return data.map(x => ({
+      ...x,
+      startedAtEst: utcStringToEstEpochMills(x.startedAt),
+      endedAtEst: utcStringToEstEpochMills(x.endedAt),
+    }));
+  }
+
   React.useEffect(() => {
     const sub$ = getTaskLogChart$()
       .subscribe(data => setList(data ?? []));
     return () => sub$.unsubscribe();
   }, []);
 
+  const minDate = new Date(Math.min(list.map(d => new Date(d.startedAt))));
+  const maxDate = new Date(Math.max(list.map(d => new Date(d.endedAt))));
+
   const config = {
     data: list,
     xField: 'id',
     yField: ['endedAt', 'startedAt'],
     colorField: 'taskName',
-    axis: {
-      x: {}
+    tooltip: {
+      fields: ['taskName', 'startedAtEst', 'endedAtEst'],
     },
     label: {
 
+    },
+
+    scrollbar: {
+      y: {}
     }
   };
 

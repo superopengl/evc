2807b8a763e21fefa91d30c9279ab242ef1e8f32
Jun Shao <mr.shaojun@gmail.com>
2025-04-28 17:57:16 +1000

    highlighting stock search

--- evc-web/src/components/StockName.js
+++ evc-web/src/components/StockName.js
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Space, Image } from 'antd';
 import { getStockLogoUrl } from 'util/getStockLogoUrl';
+import HighlightingText from 'components/HighlightingText';
 
 const { Text } = Typography;
 
 
 export const StockName = (props) => {
 
-  const { value, size, style, direction, showsLogo, logoSize, ...other } = props;
+  const { value, size, style, direction, showsLogo, logoSize, highlightenText, ...other } = props;
 
   const { symbol, company } = value;
 
   return (
     <Space direction={direction} {...other} size="small" style={{ fontSize: size }}>
-      <Text strong>{symbol}</Text>
-      {!!company && <Text type="secondary" style={{ fontSize: size, fontWeight: 300 }}>({company})</Text>}
+      <Text strong><HighlightingText search={highlightenText} value={symbol} /></Text>
+      {!!company && <Text type="secondary" style={{ fontSize: size, fontWeight: 300 }}>
+        (<HighlightingText search={highlightenText} value={company} />)
+      </Text>}
       {showsLogo && <Image src={getStockLogoUrl(symbol)}
         fallback="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC"
         width={'auto'}
@@ -32,6 +35,7 @@ StockName.propTypes = {
   direction: PropTypes.oneOf(['vertical', 'horizontal']),
   showsLogo: PropTypes.bool,
   logoSize: PropTypes.number,
+  highlightenText: PropTypes.string,
 };
 
 StockName.defaultProps = {
